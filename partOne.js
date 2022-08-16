ffb7ab951c1f232c1ffee31c19437f94c7280dc0
Ethan Groene <ethangroene@gmail.com>
2022-08-15 21:51:31 -0500

    Add foundation of startGame() function
    More functionality to be added later

--- partOne.js
+++ partOne.js
@@ -1,2 +1,10 @@
 // Add 'readline-sync' NPM package:
 var rs = require('readline-sync');
+
+// Have user press any key to begin game:
+const startGame = () => {
+    if (rs.keyIn('Press any key to start the game. ')) {
+        console.log('Let\'s play!');
+      }
+}
+startGame();
