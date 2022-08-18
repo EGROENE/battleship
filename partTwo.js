cb32af8ca023127f43ffa649c7f6091d8685f534
Ethan Groene <ethangroene@gmail.com>
2022-08-17 22:38:45 -0500

    Add logic to avoid placing the 2 units of two-unit ship in the same spot

--- partTwo.js
+++ partTwo.js
@@ -8,22 +8,45 @@ let shipLocations = [];
 
 // Function to place all ships:
 const placeAllShips = () => {
-    // Function to place 1 two-unit ship:
-    let twoUnitShip = '';
+    // Function to place 1 two-unit ship (call this once in placeAllShips(), since only one of this type is in game):
+    let twoUnitShip = [];
+    let unitOne = [];
+    let unitTwo = [];
     const placeTwoUnitShip = () => {
         let letterCoordinates = 'ABCDEFGHIJ';
         let numberCoordinates = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-        for (let i = 0; i < 2; i++) {
-            twoUnitShip += letterCoordinates[Math.floor(Math.random() * 10)] + numberCoordinates[Math.floor(Math.random() * 10)];
+
+        // Create unit one:
+        for (let i = 0; i < 1; i++) {
+            unitOne += [letterCoordinates[Math.floor(Math.random() * 10)] + numberCoordinates[Math.floor(Math.random() * 10)]];
+            console.log(unitOne);
+            twoUnitShip.push(unitOne);
         }
-        console.log(twoUnitShip);
-        shipLocations.push(twoUnitShip);
-        console.log(shipLocations);
-        return twoUnitShip;
+        // Create unit two:
+        for (let i = 0; i < 1; i++) {
+            unitTwo += [letterCoordinates[Math.floor(Math.random() * 10)] + numberCoordinates[Math.floor(Math.random() * 10)]];
+            console.log(unitTwo);
+            twoUnitShip.push(unitTwo);
+        }
+
+        // Logic preventing units from being placed in the same spot:
+        if (unitOne === unitTwo) {
+            console.log('duplicate'); // Keep now for testing purposes
+            unitOne = [];
+            unitTwo = [];
+            twoUnitShip = [];
+            console.log(twoUnitShip); // Keep now for testing purposes
+            placeTwoUnitShip();
+        } else {
+            shipLocations.push(twoUnitShip);
+            console.log(twoUnitShip); // Keep now for testing purposes
+            console.log(shipLocations); // Keep now for testing purposes
+            return twoUnitShip;
+        }
     }
     placeTwoUnitShip();
 
-    // Function to place 2 three-unit ships:
+    // Function to place 2 three-unit ships (call this function twice in placeAllShips(), as there are 2 of this type in game):
 
 
     // Function to place 1 four-unit ship:
@@ -33,6 +56,6 @@ const placeAllShips = () => {
 
 
     // Logic to prevent placing ships in same locations as others:
-    
+
 }
 placeAllShips();
