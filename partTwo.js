d2e4df16bc0a3385fa5e5ec33d8d4c27cde2193c
Ethan Groene <ethangroene@gmail.com>
2022-08-17 20:44:32 -0500

    Add placeTwoUnitShip() to placeAllShips()
    Define place at end of placeAllShips() add logic preventing ships from being placed in the same places on the grid

--- partTwo.js
+++ partTwo.js
@@ -11,8 +11,17 @@ const placeAllShips = () => {
     // Function to place 1 two-unit ship:
     let twoUnitShip = '';
     const placeTwoUnitShip = () => {
-
+        let letterCoordinates = 'ABCDEFGHIJ';
+        let numberCoordinates = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+        for (let i = 0; i < 2; i++) {
+            twoUnitShip += letterCoordinates[Math.floor(Math.random() * 10)] + numberCoordinates[Math.floor(Math.random() * 10)];
+        }
+        console.log(twoUnitShip);
+        shipLocations.push(twoUnitShip);
+        console.log(shipLocations);
+        return twoUnitShip;
     }
+    placeTwoUnitShip();
 
     // Function to place 2 three-unit ships:
 
@@ -21,4 +30,9 @@ const placeAllShips = () => {
 
 
     // Function to place 1 five-unit ship:
+
+
+    // Logic to prevent placing ships in same locations as others:
+    
 }
+placeAllShips();
